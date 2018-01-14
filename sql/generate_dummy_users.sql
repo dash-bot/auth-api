2a3dd969bb357f39d50877fd80cb7cfcb02a4f32
Charlie Friend <charles.d.friend@gmail.com>
2018-01-14 07:58:45 -0800

    Add more dummy user data.

--- sql/generate_dummy_users.sql
+++ sql/generate_dummy_users.sql
@@ -3,3 +3,28 @@
 -- Password: iliketurtles
 INSERT INTO data_users VALUES ('Charles', 'Charlie', 'Friend', now(), 'charlie.friend@teamcraps.com',
                                '\\x329279fffeb4e212d2dc75732590674f17cf9d724f220f101f20fa532c24e1e8', '0');
+
+-- User: Rhys Lawson
+-- E-mail: rhys.lawson@teamcraps.com
+-- Password: iliketurtles
+INSERT INTO data_users VALUES ('Rhys', NULL, 'Lawson', now(), 'rhys.lawson@teamcraps.com',
+                               '\\x329279fffeb4e212d2dc75732590674f17cf9d724f220f101f20fa532c24e1e8', '0');
+
+-- User: Paul Sajna
+-- E-mail: paul.sajna@teamcraps.com
+-- Password: iliketurtles
+INSERT INTO data_users VALUES ('Paul', NULL, 'Sajna', now(), 'paul.sajna@teamcraps.com',
+                               '\\x329279fffeb4e212d2dc75732590674f17cf9d724f220f101f20fa532c24e1e8', '0');
+
+-- User: Shae Brown
+-- E-mail: shae.brown@teamcraps.com
+-- Password: iliketurtles
+INSERT INTO data_users VALUES ('Shaelynn', 'Shae', 'Brown', now(), 'shae.brown@teamcraps.com',
+                               '\\x329279fffeb4e212d2dc75732590674f17cf9d724f220f101f20fa532c24e1e8', '0');
+
+-- User: AJ Po-Deziel
+-- E-mail: aj.podeziel@teamcraps.com
+-- Password: iliketurtles
+INSERT INTO data_users VALUES ('Alain-Joseph', 'AJ', 'Po-Deziel', now(), 'aj.podeziel@teamcraps.com',
+                               '\\x329279fffeb4e212d2dc75732590674f17cf9d724f220f101f20fa532c24e1e8', '0');
+
