4100e6545ee82043fead3181fd731a12b6be2575
shaebrown <shaeb@uvic.ca>
2018-01-14 01:26:29 -0800

    Speech identification API added

--- auth_api.py
+++ auth_api.py
@@ -2,10 +2,72 @@
 REST API for distributing and validating crypto-tickets for authorized users.
 Considered the authorization authority for our banking app.
 """
-from flask import Flask
+from flask import Flask, request, jsonify
+from speech_verification import VerificationServiceHttpClientHelper
+from werkzeug.utils import secure_filename
+import os
+
+suscription_key = "4a8368646beb44e29eeafd5f86ec86c9"
+speech_verification = VerificationServiceHttpClientHelper.VerificationServiceHttpClientHelper(suscription_key)
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'wav'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 application = app
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def get_wav_file(req):
+    # check if the post request has the file part
+    if 'file' not in req.files:
+        return False, "No file in request"
+    file = req.files['file']
+    print(file.filename)
+    if file.filename == '':
+        return False, "No filename in request"
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        curr_folder = os.path.dirname(os.path.abspath(__file__))
+        save_folder = os.path.join(curr_folder, app.config['UPLOAD_FOLDER'])
+        filepath = os.path.join(save_folder, filename)
+        file.save(filepath)
+        #TODO: check if wav file meets requirements
+        return True, filepath
+    return False, "File not allowed"
+
+
+@app.route('/create/speech-profile')
+def speech_create():
+    """
+    Create a new speech profile
+    :return:
+    """
+    response = speech_verification.create_profile("en-us")
+    id = response.get_profile_id()
+    # TODO do stuff with id, we are only returning for testing
+    return id
+
+
+@app.route('/enroll/speech', methods=['POST'])
+def speech_enroll():
+    """
+    Enroll profile using a voice clip. Must be done three successful times
+    :return: number of remaining enrollments
+    """
+    # TODO retrieve id from database instead of getting it from request
+    id = request.args.get("id")
+    if request.method == 'POST':
+        success, response = get_wav_file(request)
+        if not success:
+            return response
+        enroll = speech_verification.enroll_profile(id, response)
+        print(enroll.get_remaining_enrollments())
+        return str(enroll.get_remaining_enrollments())
 
 
 @app.route('/login/speech', methods=['POST'])
@@ -31,7 +93,14 @@ def speech_login():
             "error" : None or message if error
         }
     """
-    return "Not implemented"
+    # TODO retrieve id from database instead of getting it from request
+    id = request.args.get("id")
+    if request.method == 'POST':
+        success, response = get_wav_file(request)
+        if not success:
+            return response
+        verify = speech_verification.verify_file(response, id)
+        return jsonify(result=verify.get_result(), confidence=verify.get_confidence())
 
 
 @app.route("/login/text", methods=['POST'])
