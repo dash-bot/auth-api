194e3bd4657e80a29d105fde00b94585944da460
Charlie Friend <charles.d.friend@gmail.com>
2018-01-13 21:19:13 -0800

    Add configuration values for DB.

---
+++ settings.py
@@ -0,0 +1,8 @@
+import os
+
+# database url
+DB_URL = r'aa1sbw4dz894udn.cv1nhpusk2zj.us-west-2.rds.amazonaws.com'
+
+# database credentials
+DB_UNAME = os.environ["DB_UNAME"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
