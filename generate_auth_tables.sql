fed3d1df1f1332a9dbc501ae73976fdc896dc2ba
AJ Po-Deziel <aj.podeziel@gmail.com>
2018-01-13 20:48:39 -0800

    Added generate_auth_tables.sql for auth server

---
+++ generate_auth_tables.sql
@@ -0,0 +1,25 @@
+/*
+ * generate_auth_tables.sql
+ *
+ * Created on: 2018-01-14
+ *
+ * Generate tables to store user data for authentication.
+ *
+ */
+
+
+CREATE TABLE data_users (
+  first_name TEXT,
+  preferred_first_name TEXT NULL,
+  last_name TEXT,
+  created_date DATE,
+  email TEXT,
+  pwd_hash TEXT,
+  speech_profile_hash TEXT
+);
+
+CREATE TABLE data_tickets (
+  ticket_val_hash INT,
+  issued TIMESTAMP,
+  expiry TIMESTAMP
+);
